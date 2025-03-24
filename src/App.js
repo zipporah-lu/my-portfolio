bbf8431230b8903c299e58c6f7f3a2bd6e0838b7
lulong0104 <lulong850104@gmail.com>
2025-03-24 21:29:32 +1100

    240302025

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar"; 
-import Home from "./pages/Home";
+import DigitalDesign from "./pages/DigitalDesign";
 import StudyMate from "./pages/StudyMate";
 import About from "./pages/About";
 import Resume from "./pages/Resume";
@@ -10,12 +10,10 @@ function App() {
   return (
     <Router>
       <div className="min-h-screen bg-white">
-        {/* Navigation Bar */}
         <Navbar />
 
-        {/* Define Routes */}
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<DigitalDesign />} />
           <Route path="/study-mate" element={<StudyMate />} />
           <Route path="/about" element={<About />} />
           <Route path="/resume" element={<Resume />} />
