4b8b7acaabae99e9992b8934b3b7c00414d21eb1
lulong0104 <lulong850104@gmail.com>
2025-03-24 20:55:40 +1100

    24032024

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar"; 
 import Home from "./pages/Home";
-import StudyMate from "./pages/StudyMate";  // New Page
+import DigitalDesign from "./pages/DigitalDesign";
+import StudyMate from "./pages/StudyMate";
 import About from "./pages/About";
 import Resume from "./pages/Resume";
 
@@ -19,6 +20,7 @@ function App() {
           <Route path="/study-mate" element={<StudyMate />} />
           <Route path="/about" element={<About />} />
           <Route path="/resume" element={<Resume />} />
+          <Route path="/digital-design" element={<DigitalDesign />} />
         </Routes>
       </div>
     </Router>
---
+++ src/pages/DigitalDesign.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Design = () => {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold mb-4">Digital Design</h1>
+      <p>This is where I’ll showcase my digital design projects!</p>
+    </div>
+  );
+};
+
+export default Design;
