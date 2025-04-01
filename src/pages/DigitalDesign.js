7490bf106d39f47eb12a4d47d0cb5f5073eeea70
lulong0104 <lulong850104@gmail.com>
2025-04-01 22:54:14 +1100

    Update DigitalDesign.js

--- src/pages/DigitalDesign.js
+++ src/pages/DigitalDesign.js
@@ -10,19 +10,19 @@ const DigitalDesign = () => {
       ></div>
 
       {/* Content Section */}
-      <div className="bg-white px-16 py-8 rounded-lg w-full shadow-md -mt-20 max-w-6xl">
-        <h1 className="text-5xl font-bold text-gray-900 text-center">Digital Designer</h1>
+      <div className="bg-white px-20 py-10 rounded-lg w-full shadow-md -mt-20 max-w-6xl">
+        <h1 className="text-5xl font-bold text-gray-900 text-center mb-10">Digital Designer</h1>
         <div className="flex justify-between mt-4">
           <div className="flex items-center space-x-6">
             <span className="text-2xl font-semibold">Software</span>
             {['adobe-1.png', 'adobe-2.png', 'adobe-3.png', 'adobe-4.png'].map((src, index) => (
-              <img key={index} src={`${process.env.PUBLIC_URL}/${src}`} alt={src} className="w-12 h-12" />
+              <img key={index} src={`${process.env.PUBLIC_URL}/${src}`} alt={src} className="w-6 h-6" />
             ))}
           </div>
           <div className="flex items-center space-x-6">
             <span className="text-2xl font-semibold">Social Media</span>
             {['facebook.png', 'instagram.png', 'youtube.png'].map((src, index) => (
-              <img key={index} src={`${process.env.PUBLIC_URL}/${src}`} alt={src} className="w-12 h-12" />
+              <img key={index} src={`${process.env.PUBLIC_URL}/${src}`} alt={src} className="w-6 h-6" />
             ))}
           </div>
         </div>
